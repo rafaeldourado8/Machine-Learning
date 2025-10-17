4e2bb8d3c3fc68402fe50d4b1f9d47043d778af8
rafaeldourado8 <raffadrugs@gmail.com>
2025-10-17 01:01:10 +0000

    feat: Configuração inicial do ambiente e Aula 1

---
+++ 01-Fundamentos/Aula_01.py
@@ -0,0 +1,17 @@
+# Importando as bibliotecas essenciais para o curso
+
+import numpy as np
+import pandas as pd
+import matplotlib
+import seaborn as sns
+import sklearn
+
+# Imprimindo as versões para confirmar que tudo foi instalado
+
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Seaborn version:", sns.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+
+print("\nAmbiente configurado com sucesso! Tudo pronto para começar a jornada de Machine Learning.")
